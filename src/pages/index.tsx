2b224db08fab2884d723c3c11cc79c24bbd3e4b8
Elton Brayner <eltonbrayner1@gmail.com>
2022-04-21 11:05:13 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,63 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news </title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>
+              for{' '}
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              }).format(product.amount)}{' '}
+              month
+            </span>
+          </p>
+
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1J3BDNImgKXdP2OscxGvRGXt');
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // Executa 1x a cada 24h e salva a chamada
+  };
+};

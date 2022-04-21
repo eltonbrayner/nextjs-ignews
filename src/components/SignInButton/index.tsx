c8b5a28a315d4aac31f72036d3b62730f86552b1
Elton Brayner <eltonbrayner1@gmail.com>
2022-04-21 15:04:04 -0300

    Sign in button insert

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,26 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button type="button" className={styles.signInButton}>
-      <FaGithub color="#04d361" /> Elton Brayner
-      <FiX color="#737380" className={styles.closeIcon} />
+      <FaGithub color="#04d361" /> {session.user.name}
+      <FiX
+        onClick={() => signOut()}
+        color="#737380"
+        className={styles.closeIcon}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      onClick={() => signIn('github')}
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" /> Sign in with Github
     </button>
   );

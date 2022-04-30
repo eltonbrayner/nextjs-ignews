12d2466891f96724cf3c00c9f0b039bbe51b4f12
Elton Brayner <eltonbrayner1@gmail.com>
2022-04-30 11:28:13 -0300

    Configure FaunaDB on project

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +19,27 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('users_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email: user.email },
+            }),
+            q.Get(q.Match(q.Index('users_by_email'), q.Casefold(user.email))),
+          ),
+        );
+        return true;
+      } catch {
+        return;
+      }
+    },
+  },
+  secret: process.env.SIGNING_KEY,
 });
